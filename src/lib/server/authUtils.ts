e6edc2382f53d8ce378cb91912fe21856dfedf5c
Eric Branson <eric.branson@gmail.com>
2024-08-17 17:00:25 -0600

    adding password reset for lucia

--- src/lib/server/authUtils.ts
+++ src/lib/server/authUtils.ts
@@ -1,7 +1,9 @@
 import { TimeSpan, createDate } from "oslo";
-import { generateRandomString, alphabet } from "oslo/crypto";
+import { generateRandomString, alphabet, sha256 } from "oslo/crypto";
 import { isWithinExpirationDate } from "oslo";
 import prisma from "$lib/server/prisma";
+import { generateIdFromEntropySize } from "lucia";
+import { encodeHex } from "oslo/encoding";
 
 export async function generateEmailVerificationRequest(userId:string, email: string): Promise<string> {
    await prisma.verification.deleteMany({
@@ -35,4 +37,10 @@ export async function verifyEmailVerificationRequest(userId: string, userEmail: 
       return false
    }
    return true;
+}
+
+export async function passwordResetToken(userId: string): Promise<string> {
+   const tokenId = generateIdFromEntropySize(25);
+   const tokenHash = encodeHex(await sha256(new TextEncoder().encode(tokenId)));
+   await prisma.
 }
