9058894fd943dea89a25910b588d296c1df8a9e5
Eric Branson <eric.branson@gmail.com>
2024-08-18 17:41:11 -0600

    fixed copied imports

--- src/routes/login/resetPassword/+page.server.ts
+++ src/routes/login/resetPassword/+page.server.ts
@@ -1,5 +1,3 @@
-import type { PageLoad, Acctions } from './$types';
-import { lucia } from '$lib/server/lucia';
 import  prisma from "$lib/server/prisma";
 import { z } from 'zod'
 import { superValidate, message } from "sveltekit-superforms";
@@ -9,14 +7,13 @@ import { ratelimit } from "$lib/server/redis";
 
 import type { Actions, PageServerLoad } from "./$types";
 import { mailtrap } from '$lib/server/mailtrap';
-import { send } from 'vite';
-import { redirect } from '@sveltejs/kit';
+
 
 const forgotPassSchema = z.object({
    email: z.string().min(3).max(255).email().trim().toLowerCase()
 })
 
-export const load:PageLoad = (async () => {
+export const load:PageServerLoad = (async () => {
    const form = await superValidate(zod(forgotPassSchema))
 
    return {form};
