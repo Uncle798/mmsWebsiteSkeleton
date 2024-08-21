959dcd2062d8c6ea1153afd8b1c68ee634a20432
Eric Branson <eric.branson@gmail.com>
2024-08-20 20:17:08 -0600

    towards a list but need to learn components first

--- src/routes/employees/+page.svelte
+++ src/routes/employees/+page.svelte
@@ -10,45 +10,12 @@
 	import { addToast } from "$lib/Toasts/store";
 
    export let data;
-   const handler = new DataHandler(data.employees, { rowsPerPage: 50})
-   const rows = handler.getRows();
+   const { employees } = data;
 </script>
-<header>
-   <Search {handler} />
-   <RowsPerPage {handler} />
-</header>
 
-<table>
-   <thead>
-      <tr>   
-         <th>Family name</th>
-         <th>Given name</th>
-         <th>Is admin</th>
-         <th>Remove Employee</th>
-      </tr>
-      <tr>
-         <ThFilter {handler} filterBy='familyName' />
-         <ThFilter {handler} filterBy='givenName' />
-         <ThFilter {handler} filterBy='isAdmin' />
-         <th></th>
-      </tr>
-   </thead>
-   <tbody>
-      {#each $rows as row}
-         <tr>
-            <td>{row.familyName}</td>
-            <td>{row.givenName}</td>
-            <td><input type="checkbox" name="isAdmin" bind:checked={row.admin}> <button on:click={()=> 
-               addToast('Are you sure you\'d like to remove admin rights for this employee?')}>Remove Admin Rights</button></td>
-            <td>
-               <button on:click={()=> addToast({
-                  message: "Are you sure?<br/><form method='POST' action=''><input type='hidden' name='employeeId 'value={row.id}><button>Yes</button></form>",
-                  type: 'error',
-                  timeout:0,
-                  dismissible: false, 
-                  id:row.id})}>Remove Employee</button>
-            </td>
-         </tr>
-      {/each}
-   </tbody>
-</table>
+
+{#each employees as employee}
+   <div class="card">
+      {employee}
+   </div>
+{/each}
