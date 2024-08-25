be9f08e41829b4f859a6d0ca69c4bfb267634001
Eric Branson <eric.branson@gmail.com>
2024-08-25 10:28:28 -0600

    added links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import Toasts from '$lib/Toasts/Toasts.svelte';
+	import { LightSwitch, initializeStores, Drawer, Toast } from '@skeletonlabs/skeleton';
 	import "../app.css";
+	import Toasts from '$lib/Toasts/Toasts.svelte';
+	initializeStores();
 </script>
 
 <nav>
-	<a href="/login">Login</a>
+	<li>
+		<a href="/login">Login</a>
+	</li>
+	<li>
+		<a href="/logout">Logout</a>
+	</li>
+	<li>
+		<a href="/register/addressForm" class="a">Address Form</a>
+	</li>
 	<a href="/register">Register</a>
 	<a href="/units">units</a>
 	<a href="/units/available">Available units</a>
+	<a href="/units/newLease">New Lease</a>
 	<a href="/employees">employees</a>
 	<a href="/users">users</a>
+
 </nav>
 <LightSwitch />
+<Drawer />
 <Toasts />
 <slot></slot>
