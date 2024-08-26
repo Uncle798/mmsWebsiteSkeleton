78b39b44f0c5eb77a3ceeee8762df13dd686845f
Eric Branson <eric.branson@gmail.com>
2024-08-26 15:26:04 -0600

    made nav a hamburger

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,29 +2,32 @@
 	import type { PageData } from './$types';
 	import { LightSwitch, initializeStores, Drawer, Toast } from '@skeletonlabs/skeleton';
 	import "../app.css";
-	import Toasts from '$lib/Toasts/Toasts.svelte';
+	import { Hamburger } from 'svelte-hamburgers'
+	let open:boolean | undefined;
 	initializeStores();
+	export let data:PageData;
 </script>
 
+<Hamburger bind:open />
+{#if open}
+
 <nav>
-	<li>
-		<a href="/login">Login</a>
-	</li>
-	<li>
-		<a href="/logout">Logout</a>
-	</li>
-	<li>
-		<a href="/register/addressForm" class="a">Address Form</a>
-	</li>
-	<a href="/register">Register</a>
-	<a href="/units">units</a>
-	<a href="/units/available">Available units</a>
-	<a href="/units/newLease">New Lease</a>
-	<a href="/employees">employees</a>
-	<a href="/users">users</a>
+	{#if !data.user}
+		<li><a href="/login">Login</a></li>
+	{:else}
+		<li><a href="/logout">Logout</a></li>
+	{/if}
+	<li><a href="/register" class="a">Register</a></li>
+	<li><a href="/register/addressForm" class="a">Address Form</a></li>
+	<li><a href="/units">units</a></li>
+	<li><a href="/units/available">Available units</a></li>
+	<li><a href="/units/newLease">New Lease</a></li>
+	<li><a href="/employees">employees</a></li>
+	<li><a href="/users">users</a></li>
 
 </nav>
+{/if}
 <LightSwitch />
 <Drawer />
-<Toasts />
+<Toast />
 <slot></slot>
