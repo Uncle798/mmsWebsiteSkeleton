ad80fc7b9fabd98aa1c1f84a7f153bdbee0aa3f7
Eric Branson <eric.branson@gmail.com>
2024-08-27 17:05:52 -0600

    made placeholder a variable

--- src/lib/tableComponent/ThFilter.svelte
+++ src/lib/tableComponent/ThFilter.svelte
@@ -2,9 +2,10 @@
    import type { DataHandler } from "@vincjo/datatables";
    export let handler: DataHandler
    export let filterBy = '';
+   export let placeholder = "Filter"
    let value = ''
 </script>
 
 <th class="table-cell-fit">
-   <input class="input" type="text" placeholder="Filter" bind:value on:input={()=> handler.filter(value, filterBy)} />
+   <input class="input" type="text" placeholder={placeholder} bind:value on:input={()=> handler.filter(value, filterBy)} />
 </th>
