b2ef21d210e708fcc4cdce393aad76252e4d86e6
Eric Branson <eric.branson@gmail.com>
2024-09-02 10:57:35 -0600

    returning different info for employees vs customers

--- src/routes/units/[unitNum]/+page.server.ts
+++ src/routes/units/[unitNum]/+page.server.ts
@@ -8,43 +8,59 @@ export async function load(event) {
    if(!event.locals.user){
       throw redirect(302, handleLoginRedirect(event));
    }
-   const unitNum = event.params.unitNum;
-   const unitPrice = await prisma.unitPricing.findFirst({
-      where: {
-         AND:[
-            {unitNum},
-            {endDate: null}
-         ]
-      }
-   })
-   const unit = await prisma.unit.findUnique({
-      where:{
-         num:unitNum
-      }
-   })
-   const currentLease = await prisma.lease.findFirst({
-      where:{
-         AND:[
-            {unitNum},
-            {leaseEnded: null}
-         ]
-      }
-   })
-   if(unit && !currentLease){
-      return {unit, unitPrice};
-   }
-   if(unit && currentLease){
-      const currentCustomer = await prisma.contactInfo.findUnique({
-         where:{
-            contactId: currentLease?.contactInfoId
+   if(event.locals.user.employee){
+      const unitNum = event.params.unitNum;
+      const unitPrice = await prisma.unitPricing.findFirst({
+         where: {
+            AND:[
+               {unitNum},
+               {endDate: null}
+            ]
          }
       })
-      const leaseEmployee = await prisma.user.findUnique({
+      const unit = await prisma.unit.findUnique({
          where:{
-            id:currentLease?.employeeId
+            num:unitNum
          }
       })
-      return {unit, unitPrice, currentLease, currentCustomer, leaseEmployee}
+      const currentLease = await prisma.lease.findFirst({
+         where:{
+            AND:[
+               {unitNum},
+               {leaseEnded: null}
+            ]
+         }
+      })
+      if(unit && !currentLease){
+         return {unit, unitPrice};
+      }
+      if(unit && currentLease){
+         const currentCustomer = await prisma.contactInfo.findUnique({
+            where:{
+               contactId: currentLease?.contactInfoId
+            }
+         })
+         const leaseEmployee = await prisma.user.findUnique({
+            where:{
+               id:currentLease?.employeeId
+            }
+         })
+         return {unit, unitPrice, currentLease, currentCustomer, leaseEmployee}
+      }
    }
-   error(404, 'Unit not found')
+   if(event.locals.user){
+      const unitNum = event.params.unitNum;
+      const unit = prisma.unit.findFirst({
+         where: {
+            num:unitNum
+         }
+      })
+      const unitPrice = prisma.unitPricing.findFirst({
+         where:{
+            unitNum,
+         }
+      })
+      return{unit, unitPrice}
+   }
+      error(404, 'Unit not found')
 }
