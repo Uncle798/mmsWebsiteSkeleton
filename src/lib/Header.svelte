b35789a05809746f6ba0cd83331b2d4410446701
Eric Branson <eric.branson@gmail.com>
2024-09-04 14:23:40 -0600

    init it doesn't work like I hoped. I think it's svelte inside the component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+   // @ts-ignore: it works
+   import { PUBLIC_COMPANY_NAME } from '$env/static/public'
+   export let pageTitle:string;
+</script>
+
+<svelte:head>
+   <title>{PUBLIC_COMPANY_NAME} | {pageTitle}</title>
+</svelte:head>
