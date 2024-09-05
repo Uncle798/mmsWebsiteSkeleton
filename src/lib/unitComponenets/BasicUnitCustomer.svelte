2da0781dcc037bf1d6304b3026f41804b73b80b9
Eric Branson <eric.branson@gmail.com>
2024-09-05 12:39:31 -0600

    init

---
+++ src/lib/unitComponenets/BasicUnitCustomer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+   import type { Unit, UnitPricing } from 'prisma/prisma-client'
+   export let unit:Unit;
+   export let pricing:UnitPricing;
+</script>
+
+<div class="card p-4">
+   <h3>Unit: {unit.num.replace(/^0+/gm,'')}</h3>
+   <p>Building: {unit.building}</p>
+   <p>Size: {unit.size}</p>
+   <p>Price: ${pricing.price} per month</p>
+   <p>Deposit: ${pricing.price}</p>
+</div>
