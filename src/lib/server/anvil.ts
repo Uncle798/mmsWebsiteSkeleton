57e0796cfc56cb477d0112ccf79cd0523f41fb94
Eric Branson <eric.branson@gmail.com>
2024-09-05 12:39:21 -0600

    new templateId

--- src/lib/server/anvil.ts
+++ src/lib/server/anvil.ts
@@ -2,4 +2,4 @@ import Anvil from "@anvilco/anvil";
 
 export const anvilClient = new Anvil({apiKey:process.env.ANVIL_API_KEY});
 
-export const leaseTemplateId = 'juGijd5wKF03hLI00Nrd'
+export const leaseTemplateId = '3ABabYkvU2ySORZ7RKrw'
