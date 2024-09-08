131deab3d8302e38b8cd28aa73ecbabcafca8137
Eric Branson <eric.branson@gmail.com>
2024-09-08 15:37:05 -0600

    more copy explaining the process

--- src/routes/units/newlease/leaseSent/+page.svelte
+++ src/routes/units/newlease/leaseSent/+page.svelte
@@ -10,5 +10,9 @@
 	<title>{PUBLIC_COMPANY_NAME} | Lease Sent!</title>
 </svelte:head>
 
-<h1>Lease Sent!</h1>
-<h3>Please check {data.user?.email} for an email from {PUBLIC_ANVIL_EMAIL} and sign the attached lease</h3>
+<h1 class="h1">Lease Sent!</h1>
+<h3 class="h3">Please check {data.user?.email} for an email from {PUBLIC_ANVIL_EMAIL} and sign the attached lease</h3>
+<p class="p">Leases are signed after both you and a representative of { PUBLIC_COMPANY_NAME } have signed it. {PUBLIC_COMPANY_NAME} will sign contracts 
+   during business hours, and you'll be notified and sent a copy of the finalized contract. {PUBLIC_COMPANY_NAME} does not provide locks,
+   you must provide your own. 
+</p>
