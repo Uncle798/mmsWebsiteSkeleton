b4a29e7ba3ac28ef3a67ca31049f6888a3362abe
Eric Branson <eric.branson@gmail.com>
2024-09-13 13:39:57 -0600

    fixed types import now only loads for employees

--- src/routes/units/+page.server.ts
+++ src/routes/units/+page.server.ts
@@ -3,7 +3,7 @@ import  dayjs  from 'dayjs'
 import { redirect } from "@sveltejs/kit";
 import { handleLoginRedirect } from "$lib/utils";
 
-import type { PageServerLoad } from "../$types";
+import type { PageServerLoad } from "./$types";
 
 type TableData = {
    unitNum: string,
@@ -20,50 +20,55 @@ export const load:PageServerLoad = async (event) =>{
    if(!event.locals.user){
       throw redirect(302,handleLoginRedirect(event))
    }
-   const leases = await prisma.lease.findMany({
-      orderBy: {
-         unitNum: 'asc'
-      }
-   });
-   const units = await prisma.unitPricing.findMany({
-      where: {
-         endDate: null,
-      }
-   });
-   const users = await prisma.user.findMany({})
-   const tableData:TableData[] = [];
-   const today = Date.now();
-   units.forEach((unit) =>{
-      const datum:TableData = {} as TableData;
-      datum.unitNum = unit.unitNum;
-      datum.price = unit.price;
-      const unitLeases = leases.filter((lease) => lease.unitNum === unit.unitNum);
-      let shortestMonthsSinceLeaseEnded = 0;
-      let monthsSinceLeaseEnded = 0;
-      if(unitLeases){
-         for(let i=0; i<unitLeases.length; i++){
-            const leaseEndDate = unitLeases[i].leaseEnded;
-            if(!leaseEndDate) {
-               datum.familyName = users.find((user) => user.id === unitLeases[i].customerId)?.familyName;
-               datum.givenName = users.find((user) => user.id === unitLeases[i].customerId)?.givenName;
-               datum.organizationName = users.find((user) => user.id === unitLeases[i].customerId)?.organizationName;
-               datum.leasedFor = dayjs(today).diff(unitLeases[0].leaseEffectiveDate, 'months');
-               datum.emptyFor = 0;
-               break;
-            } else {
-               monthsSinceLeaseEnded = dayjs(today).diff(leaseEndDate,'months');
-            }
-            if(shortestMonthsSinceLeaseEnded === 0){
-               shortestMonthsSinceLeaseEnded = monthsSinceLeaseEnded;
-            } else if(monthsSinceLeaseEnded < shortestMonthsSinceLeaseEnded) {
-               shortestMonthsSinceLeaseEnded = monthsSinceLeaseEnded;
-            }
-            datum.emptyFor = shortestMonthsSinceLeaseEnded;
+   if(event.locals.user){
+      throw redirect(303, '/units/available')
+   }
+   if(event.locals.user.employee){
+      const leases = await prisma.lease.findMany({
+         orderBy: {
+            unitNum: 'asc'
          }
-         tableData.push(datum);
+      });
+      const units = await prisma.unitPricing.findMany({
+         where: {
+            endDate: null,
+         }
+      });
+      const users = await prisma.user.findMany({})
+      const tableData:TableData[] = [];
+      const today = Date.now();
+      units.forEach((unit) =>{
+         const datum:TableData = {} as TableData;
+         datum.unitNum = unit.unitNum;
+         datum.price = unit.price;
+         const unitLeases = leases.filter((lease) => lease.unitNum === unit.unitNum);
+         let shortestMonthsSinceLeaseEnded = 0;
+         let monthsSinceLeaseEnded = 0;
+         if(unitLeases){
+            for(let i=0; i<unitLeases.length; i++){
+               const leaseEndDate = unitLeases[i].leaseEnded;
+               if(!leaseEndDate) {
+                  datum.familyName = users.find((user) => user.id === unitLeases[i].customerId)?.familyName;
+                  datum.givenName = users.find((user) => user.id === unitLeases[i].customerId)?.givenName;
+                  datum.organizationName = users.find((user) => user.id === unitLeases[i].customerId)?.organizationName;
+                  datum.leasedFor = dayjs(today).diff(unitLeases[0].leaseEffectiveDate, 'months');
+                  datum.emptyFor = 0;
+                  break;
+               } else {
+                  monthsSinceLeaseEnded = dayjs(today).diff(leaseEndDate,'months');
+               }
+               if(shortestMonthsSinceLeaseEnded === 0){
+                  shortestMonthsSinceLeaseEnded = monthsSinceLeaseEnded;
+               } else if(monthsSinceLeaseEnded < shortestMonthsSinceLeaseEnded) {
+                  shortestMonthsSinceLeaseEnded = monthsSinceLeaseEnded;
+               }
+               datum.emptyFor = shortestMonthsSinceLeaseEnded;
+            }
+            tableData.push(datum);
+         }
+      })
+      return {
+         tableData
       }
-   })
-   return {
-      tableData
    }
 }
