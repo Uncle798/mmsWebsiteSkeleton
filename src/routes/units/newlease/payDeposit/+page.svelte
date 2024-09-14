09164128c817ffaa22870ddff602dd2a7935471c
Eric Branson <eric.branson@gmail.com>
2024-09-14 12:02:53 -0600

    stripe elements works

--- src/routes/units/newlease/payDeposit/+page.svelte
+++ src/routes/units/newlease/payDeposit/+page.svelte
@@ -3,20 +3,22 @@
    import { PUBLIC_COMPANY_NAME, PUBLIC_STRIPE_TEST } from '$env/static/public';   
    // @ts-ignore: it works
    import type { PageData } from './$types';
-   import { Elements, PaymentElement } from 'svelte-stripe';
+   import { Elements, PaymentElement, LinkAuthenticationElement, Address } from 'svelte-stripe';
    import { onMount } from 'svelte';
-	import { loadStripe } from '@stripe/stripe-js';
+	import { loadStripe, type StripeElements, type Stripe } from '@stripe/stripe-js';
 	import { enhance } from '$app/forms';
 	import { redirect } from '@sveltejs/kit';
 	export let data:PageData;
-   let stripe = null;
-   let clientSecret:string | undefined  = undefined;
-   let elements;
+   let stripe:Stripe|null;
+   let clientSecret:string;
+   let elements: StripeElements;
    let error = null;
    let processing = false;
+   let mounted = false;
    onMount(async () =>{
       stripe  = await loadStripe(PUBLIC_STRIPE_TEST);
       clientSecret = await createPaymentIntent();
+      mounted=true;
    })
    async function createPaymentIntent() {
       const response = await fetch('/stripe/paymentIntent?invoiceId='+data.invoice?.invoiceId, {
@@ -26,8 +28,7 @@
          },
          body:JSON.stringify({price:data.invoice?.price}),
       });
-      console.log('payDeposit '+response);
-      const { clientSecret } = await response.json();
+      const clientSecret = await response.json();
       return clientSecret;
    }
    async function submit() {
@@ -50,19 +51,23 @@
 <svelte:head>
 	<title>{PUBLIC_COMPANY_NAME} | Please pay your deposit</title>
 </svelte:head>
-{#if clientSecret}
-<Elements {stripe} {clientSecret} bind:elements>
-   <form on:submit|preventDefault={submit} use:enhance>
-      <PaymentElement />
-      <button class="btn" disabled={processing}>
-         {#if processing}
+{#if !mounted}
+   loading...
+{:else}
+   <Elements {stripe} clientSecret={clientSecret} bind:elements>
+      <form on:submit|preventDefault={submit} >
+         <LinkAuthenticationElement />
+         <PaymentElement />
+         <Address mode='billing' />
+         <button class="btn" disabled={processing}>
+            {#if processing}
             Processing...
             {:else}
             Pay
-         {/if}
-      </button>
-   </form>
-</Elements>
-{:else}
-   loading....
+            {/if}
+         </button>
+      </form>
+   </Elements>
+   
 {/if}
+
