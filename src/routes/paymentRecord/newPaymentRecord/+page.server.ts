c2d6d8c70cf0dde6b372decc39a6be31861189c3
Eric Branson <eric.branson@gmail.com>
2024-10-09 08:59:57 -0600

    towards a payment record workflow

--- src/routes/paymentRecord/newPaymentRecord/+page.server.ts
+++ src/routes/paymentRecord/newPaymentRecord/+page.server.ts
@@ -1,12 +1,67 @@
-import { superValidate } from 'sveltekit-superforms';
-import type { PageServerLoad } from './$types';
+import { superValidate, fail } from 'sveltekit-superforms';
+import prisma from '$lib/server/prisma';
+import { redirect } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
+import type { PageServerLoad, Actions } from './$types';
 const paymentRecordSchema = z.object({
    customerId: z.string(),
-   
+   invoiceId: z.string().nullable(),
+   paymentAmount: z.number(),
+   payee: z.string(),
+   paymentType: z.enum(['STRIPE', 'CASH', 'CHECK']),
+
 })
-export const load = (async () => {
-   const form = await superValidate(zod(paymentRecordSchema))
-   return { form, };
-}) satisfies PageServerLoad;
+export const load:PageServerLoad = (async (event) => {
+   if(!event.locals.user?.employee){
+      redirect(302, '/login');
+   }
+   const form = await superValidate( zod(paymentRecordSchema));
+   const customers = await prisma.user.findMany({
+      where: {
+         customerInvoices: {
+            some: {
+               invoicePaid: null,
+            }
+         }
+      }
+   });
+   const invoices = await prisma.invoice.findMany({
+      where: {
+         invoicePaid: null,
+      },
+      orderBy: {
+         invoiceCreated: 'desc'
+      }
+   })
+   const addresses = await prisma.contactInfo.findMany({
+      where:{
+         softDelete: false
+      }
+   })
+   return { form, customers, invoices, addresses };
+});
+
+export const actions: Actions = {
+   default: async (event) =>{
+      if(!event.locals.user?.employee){
+      redirect(302, '/login');
+      }
+      const form = await superValidate( zod(paymentRecordSchema));
+      if(!form.valid){
+         return fail(400, {form})
+      }
+      const record = await prisma.paymentRecord.create({
+         data: {
+            customerId: form.data.customerId,
+            receiverId: event.locals.user.id,
+            invoiceId: form.data.invoiceId,
+            paymentAmount: form.data.paymentAmount,
+            paymentType: form.data.paymentType,
+            payee: form.data.payee,
+         }
+      })
+      console.log(record);
+      return { form }
+   }
+};
--- src/routes/paymentRecord/newPaymentRecord/+page.svelte
+++ src/routes/paymentRecord/newPaymentRecord/+page.svelte
@@ -5,14 +5,110 @@
    import type { PageData } from './$types';
    import { Autocomplete } from '@skeletonlabs/skeleton';
    import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+   import type { ContactInfo, Invoice } from '@prisma/client';
+   import type { PartialUser } from '$lib/server/partialTypes';
+	import Address from '$lib/userComponents/Address.svelte';
+	import BasicInvoice from '$lib/leaseComponents/BasicInvoice.svelte';
     
    export let data: PageData;
-   const {form, errors, constraints, message, enhance} = superForm(data.form);
-
+   const {form, errors, constraints, message, enhance} = superForm(data.form, {
+      onUpdate(event) {
+         $form.customerId = customer?.id || '';
+      },
+   });
+   const customers = data.customers;
+   const invoices = data.invoices;
+   const addresses = data.addresses;
+   let customerSearch:string | undefined = '';
+   let customerAddress:ContactInfo[]=[];
+   let customerInvoices: Invoice[] =[];
+   let customer:PartialUser | undefined;
+   let invoiceSearch: string | undefined = '';
+   let invoiceOptions: AutocompleteOption<string>[] =[];
+   let customerOptions: AutocompleteOption<string>[] = [];
+   customers.forEach((customer)=> {
+      const customerInvoices = invoices.filter((i) => i.customerId === customer.id);
+      const option:AutocompleteOption<string> = {
+         label: `${customer.familyName}, ${customer.givenName}`,
+         value: customer.id,
+         keywords: `${customer.email}, ${customer.organizationName}`,
+      }
+      customerOptions.push(option);
+   });
+   function onCustomerSearch(event: CustomEvent<AutocompleteOption<string>>): void {
+      customerAddress.length = 0;
+      customerSearch = event.detail.label;
+      const userAddresses = addresses.filter((address) => address.userId === event.detail.value);
+      userAddresses.forEach((address) =>{
+         customerAddress.push(address);
+      });
+      customer = customers.find((user) => user.id === event.detail.value);
+      customerInvoices.length = 0;
+      const currentInvoices = invoices.filter((i) => i.customerId === customer?.id);
+      currentInvoices.forEach((cI) =>{
+         customerInvoices.push(cI)
+      })
+      invoiceOptions.length = 0;
+      currentInvoices.forEach((i) =>{
+         const option:AutocompleteOption<string> ={
+            label: String(i.invoiceNotes),
+            value: i.invoiceId,
+            keywords: `${i.invoiceNotes} ${i.invoiceAmount}`
+         }
+         invoiceOptions = [ ...invoiceOptions, option];
+      })
+      $form.customerId = event.detail.value;
+   };
+   function onInvoiceSearch(event: CustomEvent<AutocompleteOption<string>>):void {
+      invoiceSearch = event.detail.label;
+      $form.invoiceId = event.detail.value;
+   }
 </script>
 
 <div>
    <form method="post">
-      
+      <div class="card float-start">
+         <label for="customerSearch">Customers
+            <input type="search" class="input" name="customerSearch" id="customerSearch" bind:value={customerSearch} placeholder="Customer search ..." />
+            <Autocomplete bind:input={customerSearch} options={customerOptions} on:selection={onCustomerSearch} />
+         </label>
+      </div>
+      {#if invoiceOptions.length > 0}
+         
+      <div class="card float-left">
+         <input type="search" name="invoiceSearch" id="invoiceSearch" class="input" bind:value={invoiceSearch} />
+         <Autocomplete bind:input={invoiceSearch} options={invoiceOptions} on:selections = {onInvoiceSearch} />
+      </div>
+      <div class="card float-left">
+         <label for="paymentAmount">Payment Amount
+            <input type="number" name="paymentAmount" id="paymentAmount" bind:value={$form.paymentAmount} {...$constraints.paymentAmount} />
+            {#if $errors.paymentAmount}
+            {$errors.paymentAmount}
+            {/if}
+         </label>
+         <label for="payee">Payee (if different than customer)
+            <input type="text" name="payee" id="payee" bind:value={$form.payee} {...$constraints.payee} />
+            {#if $errors.payee}
+            {$errors.payee}
+            {/if}
+         </label>
+         <div>
+            <label for="paymentType">Payment Type
+               <select name="paymentType" id="paymentType" class="select" bind:value={$form.paymentType}>
+                  <option value="STRIPE">Stripe</option>
+                  <option value="CASH">Cash</option>
+                  <option value="CHECK">Check</option>
+               </select>
+            </label>
+         </div>
+      </div>
+      {/if}
+      {#if customerInvoices.length >0 }
+      {#each customerInvoices as invoice}
+      <div>
+         <BasicInvoice invoice={invoice} />
+      </div>
+         {/each}
+      {/if}
    </form>
 </div>
