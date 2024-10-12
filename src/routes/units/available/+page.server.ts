b7812201583718837615da606130339827495921
Eric Branson <eric.branson@gmail.com>
2024-10-12 17:46:50 -0600

    removing old ideas

--- src/routes/units/available/+page.server.ts
+++ src/routes/units/available/+page.server.ts
@@ -1,17 +1,9 @@
 import prisma from "$lib/server/prisma";
-import { z } from 'zod'
-import { superValidate } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters'
 
-import type { PageServerLoad, Actions } from "../$types";
+import type { PageServerLoad} from "../$types";
 import type { Unit } from "@prisma/client";
 
-const availableUnitsSchema = z.object({
-   unitId: z.string().min(23).max(30) ,
-})
-
 export const load:PageServerLoad = (async () => {
-   const form = await superValidate(zod(availableUnitsSchema));
    const units = await prisma.unit.findMany({
       orderBy: {
          num:'asc'
@@ -32,9 +24,5 @@ export const load:PageServerLoad = (async () => {
          availableUnits.push(unit);
       }
    })
-   return { form, availableUnits };
+   return { availableUnits };
 })
-
-export const actions: Actions ={
-
-}
