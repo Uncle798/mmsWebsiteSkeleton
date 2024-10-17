317a23a59876b47973af3e6280366db48dbcdb32
Eric Branson <eric.branson@gmail.com>
2024-10-17 10:39:48 -0600

    init

---
+++ src/lib/formComponents/TextInput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+   import type { InputConstraint } from 'sveltekit-superforms';
+ 
+   export let value: string | undefined = undefined;
+   export let label: string | undefined = undefined;
+   export let placeholder: string | undefined = undefined;
+   export let errors: string[] | undefined = undefined;
+   export let constraints: InputConstraint | undefined = undefined;
+ </script>
+ 
+ <label>
+   {#if label}<span>{label}</span><br />{/if}
+   <input
+      type="text"
+      class="input"
+      placeholder={placeholder ? placeholder : undefined}
+      bind:value
+      aria-invalid={errors ? 'true' : undefined}
+      {...constraints}
+      {...$$restProps} 
+      />
+ </label>
+ {#if errors}<span class="invalid">{errors}</span>{/if}
