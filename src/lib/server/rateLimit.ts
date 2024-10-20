6983ca2ac2b56d88ef38c85208ff6de7685dbc02
Eric Branson <eric.branson@gmail.com>
2024-10-20 10:56:06 -0600

    better name

--- src/lib/server/rateLimit.ts
+++ src/lib/server/rateLimit.ts
@@ -1,7 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 
-
 const redis = new Redis({
    url: `https://${process.env.UPSTASH_ENDPOINT}`,
    token: process.env.UPSTASH_PASSWORD,
