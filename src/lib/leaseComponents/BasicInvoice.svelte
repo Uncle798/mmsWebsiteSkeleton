e4db54173afd1ed64b5e39aeda83f7f823519c87
Eric Branson <eric.branson@gmail.com>
2024-10-21 13:45:55 -0600

    date formats

--- src/lib/leaseComponents/BasicInvoice.svelte
+++ src/lib/leaseComponents/BasicInvoice.svelte
@@ -4,10 +4,10 @@
    export let invoice:Invoice;
 </script>
 <div class="card">
-   <p>Invoice created: {dayjs(invoice.invoiceCreated).format('MMMM YYYY')}</p>
+   <p>Invoice created: {dayjs(invoice.invoiceCreated).format('M/D/YYYY')}</p>
    <p>Invoice amount: ${invoice.invoiceAmount}</p>
    {#if invoice.invoicePaid}
-      <p>Invoice paid: {dayjs(invoice.invoicePaid).format('MMMM YYYY')}</p>
+      <p>Invoice paid: {dayjs(invoice.invoicePaid).format('M/D/YYYY')}</p>
    {/if}
    <p>Invoice notes: {invoice.invoiceNotes}</p>
 </div>
--- src/lib/leaseComponents/BasicLease.svelte
+++ src/lib/leaseComponents/BasicLease.svelte
@@ -11,8 +11,8 @@
       <h3 class="h3">Unit Number: <a href="/units/{lease.unitNum}"> {lease.unitNum.replace(/^0+/gm,'')}</a></h3>
       <h4 class="h4">Price: ${lease.price}</h4>
    </span>
-   <p>Lease effective Date: {dayjs(lease.leaseEffectiveDate).format('DD/MM/YYYY')}</p>
+   <p>Lease effective Date: {dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</p>
    {#if lease.leaseEnded}
-      <p>Lease ended: {dayjs(lease.leaseEnded).format('DD/MM/YYYY')}</p>
+      <p>Lease ended: {dayjs(lease.leaseEnded).format('M/D/YYYY')}</p>
    {/if}
 </div>
